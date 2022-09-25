af971aba7b32daa45ea85ebc1db2bb05eb9c5225
Ted Pattison <ted@tedpattison.net>
2022-09-25 13:39:59 -0400

    Updates

---
+++ AppOwnsDataAdmin/Controllers/HomeController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AppOwnsDataShared.Models;
+using AppOwnsDataShared.Services;
+using AppOwnsDataAdmin.Models;
+using AppOwnsDataAdmin.Services;
+
+namespace AppOwnsDataAdmin.Controllers {
+
+  [Authorize]
+  public class HomeController : Controller {
+
+    private PowerBiServiceApi powerBiServiceApi;
+    private AppOwnsDataDBService AppOwnsDataDBService;
+
+    public HomeController(PowerBiServiceApi powerBiServiceApi, AppOwnsDataDBService AppOwnsDataDBService) {
+      this.powerBiServiceApi = powerBiServiceApi;
+      this.AppOwnsDataDBService = AppOwnsDataDBService;
+    }
+
+    [AllowAnonymous]
+    public IActionResult Index() {
+      return View();
+    }
+
+    public IActionResult Tenants() {
+      var model = AppOwnsDataDBService.GetTenants();
+      return View(model);
+    }
+
+    public IActionResult Tenant(string Name) {
+      var model = AppOwnsDataDBService.GetTenant(Name);
+      var modelWithDetails = powerBiServiceApi.GetTenantDetails(model);
+      return View(modelWithDetails);
+    }
+
+    public class OnboardTenantModel {
+      public string TenantName { get; set; }
+      public string SuggestedDatabase { get; set; }
+      public List<SelectListItem> DatabaseOptions { get; set; }
+      public string SuggestedAppIdentity { get; set; }
+      public List<SelectListItem> AppIdentityOptions { get; set; }
+    }
+
+    public IActionResult OnboardTenant() {
+
+      var model = new OnboardTenantModel {
+        TenantName = this.AppOwnsDataDBService.GetNextTenantName(),
+        SuggestedDatabase = "WingtipSales",
+        DatabaseOptions = new List<SelectListItem> {
+          new SelectListItem{ Text="AcmeCorpSales", Value="AcmeCorpSales" },
+          new SelectListItem{ Text="ContosoSales", Value="ContosoSales" },
+          new SelectListItem{ Text="MegaCorpSales", Value="MegaCorpSales" }
+        }
+      };
+
+      return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult OnboardTenant(string TenantName, string DatabaseServer, string DatabaseName, string DatabaseUserName, string DatabaseUserPassword) {
+
+      var tenant = new PowerBiTenant {
+        Name = TenantName,
+        DatabaseServer = DatabaseServer,
+        DatabaseName = DatabaseName,
+        DatabaseUserName = DatabaseUserName,
+        DatabaseUserPassword = DatabaseUserPassword,
+      };    
+
+      tenant = this.powerBiServiceApi.OnboardNewTenant(tenant);
+      this.AppOwnsDataDBService.OnboardNewTenant(tenant);
+
+      return RedirectToAction("Tenants");
+
+    }
+
+    public IActionResult DeleteTenant(string TenantName) {
+      var tenant = this.AppOwnsDataDBService.GetTenant(TenantName);
+      this.powerBiServiceApi.DeleteTenant(tenant);
+      this.AppOwnsDataDBService.DeleteTenant(tenant);
+      return RedirectToAction("Tenants");
+    }
+
+    public IActionResult Embed(string TenantName) {
+      var tenant = this.AppOwnsDataDBService.GetTenant(TenantName);
+      var viewModel = this.powerBiServiceApi.GetReportEmbeddingData(tenant).Result;
+      return View(viewModel);
+    }
+
+    public IActionResult Users() {
+      var model = AppOwnsDataDBService.GetUsers();
+      return View(model);
+    }
+   
+    public IActionResult GetUser(string LoginId) {
+      var model = AppOwnsDataDBService.GetUser(LoginId);
+      return View(model);
+    }
+
+    public class EditUserModel {
+      public User User { get; set; }
+      public List<SelectListItem> TenantOptions { get; set; }
+    }
+
+    public IActionResult EditUser(string LoginId) {
+      var model = new EditUserModel{
+        User = AppOwnsDataDBService.GetUser(LoginId),
+        TenantOptions = this.AppOwnsDataDBService.GetTenants().Select(tenant => new SelectListItem {
+                Text = tenant.Name,
+                Value = tenant.Name
+              }).ToList(),
+      };
+      return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult EditUser(User user) {
+      var model = AppOwnsDataDBService.UpdateUser(user);
+      return RedirectToAction("Users");
+    }
+
+    public class CreateUserModel {
+      public List<SelectListItem> TenantOptions { get; set; }
+    }
+
+    public IActionResult CreateUser() {
+      var model = new CreateUserModel {
+        TenantOptions = this.AppOwnsDataDBService.GetTenants().Select(tenant => new SelectListItem {
+          Text = tenant.Name,
+          Value = tenant.Name
+        }).ToList(),
+      };
+      return View(model);
+    }
+
+    [HttpPost]
+    public IActionResult CreateUser(User user) {
+      var model = AppOwnsDataDBService.CreateUser(user);
+      return RedirectToAction("Users");
+    }
+
+    public IActionResult DeleteUser(string LoginId) {
+      var user = this.AppOwnsDataDBService.GetUser(LoginId);
+      this.AppOwnsDataDBService.DeleteUser(user);
+      return RedirectToAction("Users");
+    }
+
+    public IActionResult ActivityLog() {
+      var model = AppOwnsDataDBService.GetActivityLog();
+      return View(model);
+    }
+
+    public IActionResult ActivityEvent(int id) {
+      ActivityLogEntry model = AppOwnsDataDBService.GetActivityLogEntry(id);
+      return View(model);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
---
+++ AppOwnsDataAdmin/Services/PowerBiServiceApi.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.PowerBI.Api;
+using Microsoft.PowerBI.Api.Models;
+using Microsoft.PowerBI.Api.Models.Credentials;
+using Microsoft.Rest;
+using AppOwnsDataShared.Models;
+using Microsoft.Identity.Web;
+using AppOwnsDataShared.Services;
+using System.Text;
+
+namespace AppOwnsDataAdmin.Services {
+
+  public class EmbeddedReportViewModel {
+    public string ReportId;
+    public string Name;
+    public string EmbedUrl;
+    public string Token;
+    public string TenantName;
+  }
+
+  public class PowerBiTenantDetails : PowerBiTenant {
+    public IList<Report> Reports { get; set; }
+    public IList<Dataset> Datasets { get; set; }
+    public IList<GroupUser> Members { get; set; }
+  }
+
+  public class PowerBiServiceApi {
+
+    private readonly AppOwnsDataDBService AppOwnsDataDBService;
+    private readonly IConfiguration Configuration;
+    private readonly IWebHostEnvironment Env;
+
+    private ITokenAcquisition tokenAcquisition { get; }
+    private string urlPowerBiServiceApiRoot { get; }
+    private string targetCapacityId { get;  }
+    private PowerBIClient pbiClient { get; set; }
+
+
+    public PowerBiServiceApi(IConfiguration configuration, ITokenAcquisition tokenAcquisition, AppOwnsDataDBService AppOwnsDataDBService, IWebHostEnvironment env) {
+      this.Configuration = configuration;
+      this.urlPowerBiServiceApiRoot = configuration["PowerBi:ServiceRootUrl"];
+      this.targetCapacityId = configuration["PowerBi:TargetCapacityId"];
+      this.tokenAcquisition = tokenAcquisition;
+      this.AppOwnsDataDBService = AppOwnsDataDBService;
+      this.Env = env;
+      pbiClient = GetPowerBiClient();
+    }
+
+    public const string powerbiApiDefaultScope = "https://analysis.windows.net/powerbi/api/.default";
+
+    public string GetAccessToken() {
+      return this.tokenAcquisition.GetAccessTokenForAppAsync(powerbiApiDefaultScope).Result;
+    }
+
+    public PowerBIClient GetPowerBiClient() {
+      var tokenCredentials = new TokenCredentials(GetAccessToken(), "Bearer");
+      return new PowerBIClient(new Uri(urlPowerBiServiceApiRoot), tokenCredentials);
+    }
+
+    private PowerBIClient GetPowerBiClientForProfile(Guid ProfileId) {
+      var tokenCredentials = new TokenCredentials(GetAccessToken(), "Bearer");
+      return new PowerBIClient(new Uri(urlPowerBiServiceApiRoot), tokenCredentials, ProfileId);
+    }
+
+    private void SetCallingContext(string ProfileId = "") {
+
+      if (ProfileId.Equals("")) {
+        pbiClient = GetPowerBiClient();
+      }
+      else {
+        pbiClient = GetPowerBiClientForProfile(new Guid(ProfileId));
+      }
+
+    }
+
+
+    public async Task<EmbeddedReportViewModel> GetReport(Guid WorkspaceId, Guid ReportId) {
+
+      // call to Power BI Service API to get embedding data
+      var report = await pbiClient.Reports.GetReportInGroupAsync(WorkspaceId, ReportId);
+
+      // generate read-only embed token for the report
+      var datasetId = report.DatasetId;
+      var tokenRequest = new GenerateTokenRequest(TokenAccessLevel.View, datasetId);
+      var embedTokenResponse = await pbiClient.Reports.GenerateTokenAsync(WorkspaceId, ReportId, tokenRequest);
+      var embedToken = embedTokenResponse.Token;
+
+      // return report embedding data to caller
+      return new EmbeddedReportViewModel {
+        ReportId = report.Id.ToString(),
+        EmbedUrl = report.EmbedUrl,
+        Name = report.Name,
+        Token = embedToken
+      };
+    }
+
+    public Dataset GetDataset(Guid WorkspaceId, string DatasetName) {
+      var datasets = pbiClient.Datasets.GetDatasetsInGroup(WorkspaceId).Value;
+      foreach (var dataset in datasets) {
+        if (dataset.Name.Equals(DatasetName)) {
+          return dataset;
+        }
+      }
+      return null;
+    }
+
+    public async Task<IList<Group>> GetTenantWorkspaces() {
+      var workspaces = (await pbiClient.Groups.GetGroupsAsync()).Value;
+      return workspaces;
+    }
+
+    public PowerBiTenant OnboardNewTenant(PowerBiTenant tenant) {
+
+      this.SetCallingContext();
+
+      var createRequest = new CreateOrUpdateProfileRequest(tenant.Name + " Profile");
+      var profile = pbiClient.Profiles.CreateProfile(createRequest);
+
+      string profileId = profile.Id.ToString();
+      tenant.ProfileId = profileId;
+
+      SetCallingContext(profileId);
+
+      // create new app workspace
+      GroupCreationRequest request = new GroupCreationRequest("AODSK: " + tenant.Name);
+      Group workspace = pbiClient.Groups.CreateGroup(request);
+
+      // assign new workspace to dedicated capacity 
+      if(targetCapacityId != "") {
+        pbiClient.Groups.AssignToCapacity(workspace.Id, new AssignToCapacityRequest {
+          CapacityId = new Guid(targetCapacityId),
+        });
+      }
+
+      tenant.WorkspaceId = workspace.Id.ToString();
+      tenant.WorkspaceUrl = "https://app.powerbi.com/groups/" + workspace.Id.ToString() + "/";
+
+      // add user as new workspace admin to make demoing easier
+      string adminUser = Configuration["DemoSettings:AdminUser"];
+      if (!string.IsNullOrEmpty(adminUser)) {
+        pbiClient.Groups.AddGroupUser(workspace.Id, new GroupUser {
+          Identifier = adminUser,
+          PrincipalType = PrincipalType.User,
+          EmailAddress = adminUser,
+          GroupUserAccessRight = "Admin"
+        });
+      }
+
+      // 
+      pbiClient.Groups.AddGroupUser(workspace.Id, new GroupUser {
+        Identifier = "2667e2f2-ea90-4ce0-8e7c-f4bb72cae75c",
+        PrincipalType = PrincipalType.App,        
+        GroupUserAccessRight = "Contributor"
+      });
+
+      // upload sample PBIX file #1
+      string importName = "Sales";
+      PublishPBIX(workspace.Id, importName);
+
+      Dataset dataset = GetDataset(workspace.Id, importName);
+
+      UpdateMashupParametersRequest req =
+        new UpdateMashupParametersRequest(new List<UpdateMashupParameterDetails>() {
+          new UpdateMashupParameterDetails { Name = "DatabaseServer", NewValue = tenant.DatabaseServer },
+          new UpdateMashupParameterDetails { Name = "DatabaseName", NewValue = tenant.DatabaseName }
+      });
+
+      pbiClient.Datasets.UpdateParametersInGroup(workspace.Id, dataset.Id, req);
+
+      PatchSqlDatasourceCredentials(workspace.Id, dataset.Id, tenant.DatabaseUserName, tenant.DatabaseUserPassword);
+
+      pbiClient.Datasets.RefreshDatasetInGroup(workspace.Id, dataset.Id);
+
+      string PaginatedReportName = "Sales Summary";
+      PublishRDL(workspace, PaginatedReportName, dataset);
+
+      return tenant;
+    }
+
+    public PowerBiTenantDetails GetTenantDetails(PowerBiTenant tenant) {
+
+      SetCallingContext(tenant.ProfileId);
+
+      return new PowerBiTenantDetails {
+        Name = tenant.Name,
+        DatabaseName = tenant.DatabaseName,
+        DatabaseServer = tenant.DatabaseServer,
+        DatabaseUserName = tenant.DatabaseUserName,
+        DatabaseUserPassword = tenant.DatabaseUserPassword,
+        ProfileId = tenant.ProfileId,
+        Created = tenant.Created,
+        WorkspaceId = tenant.WorkspaceId,
+        WorkspaceUrl = tenant.WorkspaceUrl,
+        Members = pbiClient.Groups.GetGroupUsers(new Guid(tenant.WorkspaceId)).Value,
+        Datasets = pbiClient.Datasets.GetDatasetsInGroup(new Guid(tenant.WorkspaceId)).Value,
+        Reports = pbiClient.Reports.GetReportsInGroup(new Guid(tenant.WorkspaceId)).Value
+      };
+
+    }
+
+    public void DeleteTenant(PowerBiTenant tenant) {
+
+      // delete workspace as service principal profile
+      SetCallingContext(tenant.ProfileId);
+      Guid workspaceIdGuid = new Guid(tenant.WorkspaceId);
+      pbiClient.Groups.DeleteGroup(workspaceIdGuid);
+
+      // swtch back to service principal to delete service principal profile
+      SetCallingContext();
+      pbiClient.Profiles.DeleteProfile(new Guid(tenant.ProfileId));
+ 
+    }
+
+    public void PublishPBIX(Guid WorkspaceId, string ImportName) {
+
+      string PbixFilePath = this.Env.WebRootPath + @"/PBIX/SalesReportTemplate.pbix";
+
+      FileStream stream = new FileStream(PbixFilePath, FileMode.Open, FileAccess.Read);
+
+      var import = pbiClient.Imports.PostImportWithFileInGroup(WorkspaceId, stream, ImportName);
+
+      while (import.ImportState != "Succeeded") {
+        import = pbiClient.Imports.GetImportInGroup(WorkspaceId, import.Id);
+      }
+
+    }
+
+    public void PatchSqlDatasourceCredentials(Guid WorkspaceId, string DatasetId, string SqlUserName, string SqlUserPassword) {
+
+      var datasources = (pbiClient.Datasets.GetDatasourcesInGroup(WorkspaceId, DatasetId)).Value;
+
+      // find the target SQL datasource
+      foreach (var datasource in datasources) {
+        if (datasource.DatasourceType.ToLower() == "sql") {
+          // get the datasourceId and the gatewayId
+          var datasourceId = datasource.DatasourceId;
+          var gatewayId = datasource.GatewayId;
+          // Create UpdateDatasourceRequest to update Azure SQL datasource credentials
+          UpdateDatasourceRequest req = new UpdateDatasourceRequest {
+            CredentialDetails = new CredentialDetails(
+              new BasicCredentials(SqlUserName, SqlUserPassword),
+              PrivacyLevel.None,
+              EncryptedConnection.NotEncrypted)
+          };
+          // Execute Patch command to update Azure SQL datasource credentials
+          pbiClient.Gateways.UpdateDatasource((Guid)gatewayId, (Guid)datasourceId, req);
+        }
+      };
+
+    }
+
+    public void PublishRDL(Group Workspace, string ImportName, Dataset TargetDataset) {
+      
+      string rdlFilePath = this.Env.WebRootPath + @"/PBIX/SalesSummaryPaginated.rdl";
+
+      FileStream stream = new FileStream(rdlFilePath, FileMode.Open, FileAccess.Read);
+      StreamReader reader = new StreamReader(stream);
+      string rdlFileContent = reader.ReadToEnd();
+      reader.Close();
+      stream.Close();
+
+      rdlFileContent = rdlFileContent.Replace("{{TargetDatasetId}}", TargetDataset.Id.ToString())
+                                     .Replace("{{PowerBIWorkspaceName}}",Workspace.Name )
+                                     .Replace("{{PowerBIDatasetName}}", TargetDataset.Name);
+
+      MemoryStream contentSteam = new MemoryStream(Encoding.ASCII.GetBytes(rdlFileContent));
+
+      string rdlImportName = ImportName + ".rdl";
+
+      var import = pbiClient.Imports.PostImportWithFileInGroup(Workspace.Id, contentSteam, rdlImportName, ImportConflictHandlerMode.Abort);
+
+      // poll to determine when import operation has complete
+      do { import = pbiClient.Imports.GetImportInGroup(Workspace.Id, import.Id); }
+      while (import.ImportState.Equals("Publishing"));
+
+      Guid reportId = import.Reports[0].Id;
+
+      UpdatePaginatedReportDatasource(Workspace.Id, reportId, TargetDataset.Id);
+
+    }
+
+    public void UpdatePaginatedReportDatasource(Guid WorkspaceId, Guid ReportId, string DatasetId) {
+
+      var report = pbiClient.Reports.GetReportInGroup(WorkspaceId, ReportId);
+
+      var reportDatasources = pbiClient.Reports.GetDatasourcesInGroup(WorkspaceId, report.Id);
+      Datasource reportDatasource = reportDatasources.Value[0];
+
+
+      string Server = "pbiazure://api.powerbi.com/";
+      string Database = "sobe_wowvirtualserver-" + DatasetId;
+
+      UpdateRdlDatasourceDetails updateRdlDatasourceDetails = new UpdateRdlDatasourceDetails {
+        DatasourceName = "PowerBiDataset",
+        ConnectionDetails = new RdlDatasourceConnectionDetails {
+          Server = Server,
+          Database = Database
+        }
+      };
+
+      List<UpdateRdlDatasourceDetails> updateRdlDatasourceDetailsList = new List<UpdateRdlDatasourceDetails>();
+      updateRdlDatasourceDetailsList.Add(updateRdlDatasourceDetails);
+      UpdateRdlDatasourcesRequest updateDatasourcesRequest = new UpdateRdlDatasourcesRequest(updateRdlDatasourceDetailsList);
+      pbiClient.Reports.UpdateDatasourcesInGroup(WorkspaceId, report.Id, updateDatasourcesRequest);
+
+      //PatchDatasetAsDatasourceCredentials(WorkspaceId, DatasetId);
+
+    }
+
+    public void PatchDatasetAsDatasourceCredentials(Guid WorkspaceId, string DatasetId) {
+
+
+      var datasources = (pbiClient.Datasets.GetDatasourcesInGroup(WorkspaceId, DatasetId)).Value;
+      var datasource = datasources[0];
+
+      var datasourceId = datasource.DatasourceId;
+      var gatewayId = datasource.GatewayId;
+
+      UpdateDatasourceRequest req = new UpdateDatasourceRequest {
+        CredentialDetails = new CredentialDetails {
+          CredentialType = CredentialType.OAuth2,
+          UseCallerAADIdentity = true,
+          EncryptedConnection = EncryptedConnection.NotEncrypted,
+          EncryptionAlgorithm = EncryptionAlgorithm.None,
+          PrivacyLevel = PrivacyLevel.None,
+          UseEndUserOAuth2Credentials = true
+        }
+      };
+
+      // Execute Patch command to update Azure SQL datasource credentials
+      pbiClient.Gateways.UpdateDatasource((Guid)gatewayId, (Guid)datasourceId, req);
+
+    }
+
+
+    public async Task<EmbeddedReportViewModel> GetReportEmbeddingData(PowerBiTenant Tenant) {
+
+      SetCallingContext(Tenant.ProfileId);
+
+      Guid workspaceId = new Guid(Tenant.WorkspaceId);
+      var reports = (await pbiClient.Reports.GetReportsInGroupAsync(workspaceId)).Value;
+
+      var report = reports.Where(report => report.Name.Equals("Sales")).First();
+
+      GenerateTokenRequest generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "View");
+
+      // call to Power BI Service API and pass GenerateTokenRequest object to generate embed token
+      string embedToken = pbiClient.Reports.GenerateTokenInGroup(workspaceId, report.Id,
+                                                                 generateTokenRequestParameters).Token;
+
+      return new EmbeddedReportViewModel {
+        ReportId = report.Id.ToString(),
+        Name = report.Name,
+        EmbedUrl = report.EmbedUrl,
+        Token = embedToken,
+        TenantName = Tenant.Name
+      };
+
+    }
+
+
+  }
+
+}
