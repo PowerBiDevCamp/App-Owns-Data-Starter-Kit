9e6dec72769947d1dc7e477cfdedcc64dd29a7a1
Ted Pattison <ted@tedpattison.net>
2022-09-25 10:26:05 -0400

    Updates

--- AppOwnsDataAdmin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace AppOwnsDataAdmin.Models {
-  public class ErrorViewModel {
-    public string RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-  }
-}
--- AppOwnsDataClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace AppOwnsDataClient.Models {
-  public class ErrorViewModel {
-    public string RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-  }
-}
