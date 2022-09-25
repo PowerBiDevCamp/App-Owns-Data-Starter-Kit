4b8a95bd1bc95ca9f59c318a4a20e09f8452742e
Ted Pattison <ted@tedpattison.net>
2022-09-25 14:55:34 -0400

    Update appSettings.ts

--- AppOwnsDataClient/App/appSettings.ts
+++ AppOwnsDataClient/App/appSettings.ts
@@ -3,7 +3,7 @@
   // replace with production URL after deploying Web API
   public static apiRoot: string = "https://localhost:44302/api/"; 
 
-  // setting for Azure AD app with supports SPA authentication
+  // setting for Azure AD app which supports SPA authentication
   public static tenant: string = "11111111-1111-1111-1111-111111111111";
   public static clientId: string = "22222222-2222-2222-2222-222222222222";
  
