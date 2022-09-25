7f8e2b308b0f9912828990445068ceef1cf821ca
Ted Pattison <ted@tedpattison.net>
2022-09-25 14:31:25 -0400

    Updates

---
+++ AppOwnsDataShared/Models/AppOwnsDataDB.cs
@@ -0,0 +1,83 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+
+namespace AppOwnsDataShared.Models {
+
+  public class PowerBiTenant {
+    [Key]
+    public string Name { get; set; }
+    public string ProfileId { get; set; }
+    public string WorkspaceId { get; set; }
+    public string WorkspaceUrl { get; set; }
+    public string DatabaseServer { get; set; }
+    public string DatabaseName { get; set; }
+    public string DatabaseUserName { get; set; }
+    public string DatabaseUserPassword { get; set; }
+    public DateTime Created { get; set; }
+  }
+
+  public class User {
+    [Key]
+    public string LoginId { get; set; }
+    public string UserName { get; set; }
+    public bool CanEdit { get; set; }
+    public bool CanCreate { get; set; }
+    public bool TenantAdmin { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastLogin { get; set; }
+    public string TenantName { get; set; }
+  }
+
+  public class ActivityLogEntry {
+    public int Id { get; set; }
+    public DateTime Created { get; set; }
+    public string LoginId { get; set; }
+    public string User { get; set; }
+    public string Activity { get; set; }
+    public string Tenant { get; set; }
+    public string WorkspaceId { get; set; }
+    public string Dataset { get; set; }
+    public string DatasetId { get; set; }
+    public string Report { get; set; }
+    public string ReportId { get; set; }
+    public string ReportType { get; set; }
+    public string PageName { get; set; }
+    public string OriginalReportId { get; set; }
+    public int? LoadDuration { get; set; }
+    public int? RenderDuration { get; set; }
+    public string CorrelationId { get; set; }
+    public string EmbedTokenId { get; set; }
+  }
+
+  public class AppOwnsDataDB : DbContext {
+
+    public AppOwnsDataDB(DbContextOptions<AppOwnsDataDB> options) : base(options) { }
+
+    public DbSet<PowerBiTenant> Tenants { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<ActivityLogEntry> ActivityLog { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+      base.OnModelCreating(modelBuilder);
+    }
+
+  }
+
+  public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppOwnsDataDB> {
+    public AppOwnsDataDB CreateDbContext(string[] args) {
+      string configFilePath = @Directory.GetCurrentDirectory() + "/../AppOwnsDataAdmin/appsettings.json";
+      IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(configFilePath).Build();
+      var builder = new DbContextOptionsBuilder<AppOwnsDataDB>();
+      builder.UseSqlServer(configuration["AppOwnsDataDB:ConnectString"]);
+      return new AppOwnsDataDB(builder.Options);
+    }
+  }
+
+}
---
+++ AppOwnsDataShared/Services/AppOwnsDataDBService.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AppOwnsDataShared.Models;
+
+namespace AppOwnsDataShared.Services {
+
+  public class AppOwnsDataDBService {
+
+    private readonly AppOwnsDataDB dbContext;
+
+    public AppOwnsDataDBService(AppOwnsDataDB context) {
+      dbContext = context;
+    }
+
+    public string GetNextTenantName() {
+      var appNames = dbContext.Tenants.Select(tenant => tenant.Name).ToList();
+      string baseName = "Tenant";
+      string nextName;
+      int counter = 0;
+      do {
+        counter += 1;
+        nextName = baseName + counter.ToString("00");
+      }
+      while (appNames.Contains(nextName));
+      return nextName;
+    }
+
+    public void OnboardNewTenant(PowerBiTenant tenant) {
+      tenant.Created = DateTime.Now;
+      dbContext.Tenants.Add(tenant);
+      dbContext.SaveChanges();
+    }
+
+    public IList<PowerBiTenant> GetTenants() {
+      return dbContext.Tenants
+             .Select(tenant => tenant)
+             .OrderBy(tenant => tenant.Name)
+             .ToList();
+    }
+
+    public PowerBiTenant GetTenant(string TenantName) {
+      var tenant = dbContext.Tenants.Where(tenant => tenant.Name == TenantName).First();
+      return tenant;
+    }
+
+    public void DeleteTenant(PowerBiTenant tenant) {
+
+      // unassign any users in the tenant
+      var tenantUsers = dbContext.Users.Where(user => user.TenantName == tenant.Name);
+      foreach (var user in tenantUsers) {
+        user.TenantName = "";
+        dbContext.Users.Update(user);
+      }
+      dbContext.SaveChanges();
+
+      // delete the tenant
+      dbContext.Tenants.Remove(tenant);
+      dbContext.SaveChanges();
+      return;
+    }
+
+    public IList<User> GetUsers() {
+      return dbContext.Users
+             .Select(user => user)
+             .OrderByDescending(user => user.LastLogin)
+             .ToList();
+    }
+
+    public User GetUser(string LoginId) {
+      var user = dbContext.Users.Where(user => user.LoginId == LoginId).First();
+      return user;
+    }
+
+    public User UpdateUser(User currentUser) {
+      var users = dbContext.Users.Where(user => user.LoginId == currentUser.LoginId);
+      User user;
+      if (users.Count() > 0) {
+        user = users.First();
+      }
+      else {
+        user = new User();
+      }
+      user.UserName = currentUser.UserName;
+      user.CanEdit = currentUser.CanEdit;
+      user.CanCreate = currentUser.CanCreate;
+      user.TenantName = currentUser.TenantName;
+      dbContext.SaveChanges();
+      return user;
+    }
+
+    public User CreateUser(User newUser) {
+      var users = dbContext.Users.Where(user => user.LoginId == newUser.LoginId);
+      User user;
+      if (users.Count() > 0) {
+        user = users.First();
+      }
+      else {
+        user = new User();
+        user.Created = DateTime.Now;
+      }
+      user.LoginId = newUser.LoginId;
+      user.UserName = !string.IsNullOrEmpty(newUser.UserName) ? newUser.UserName : user.UserName;
+      user.TenantName = !string.IsNullOrEmpty(newUser.TenantName) ? newUser.TenantName : user.TenantName;
+      user.CanEdit = newUser.CanEdit;
+      user.CanCreate = newUser.CanCreate;
+      user.LastLogin = DateTime.Now;
+      dbContext.Users.Add(user);
+      dbContext.SaveChanges();
+      return user;
+    }
+
+    public void DeleteUser(User user) {
+      dbContext.Users.Remove(user);
+      dbContext.SaveChanges();
+      return;
+    }
+
+    public List<ActivityLogEntry> GetActivityLog() {
+      return this.dbContext.ActivityLog
+        .OrderByDescending(entry => entry.Created).Take(50)
+        .ToList();
+    }
+
+    public ActivityLogEntry GetActivityLogEntry(int id) {
+      return this.dbContext.ActivityLog.Find(id);
+    }
+
+    public ActivityLogEntry PostActivityLogEntry(ActivityLogEntry activityLogEntry) {
+      activityLogEntry.Created = DateTime.Now;
+      activityLogEntry.WorkspaceId = (dbContext.Tenants.Where(tenant => tenant.Name == activityLogEntry.Tenant).First()).WorkspaceId;
+      dbContext.ActivityLog.Add(activityLogEntry);
+      dbContext.SaveChanges();
+      return activityLogEntry;
+    }
+
+    public void ProcessUserLogin(User currentUser) {
+
+      bool userExists = this.dbContext.Users.Any(user => user.LoginId == currentUser.LoginId);
+
+      if (userExists) {
+        currentUser = dbContext.Users.Find(currentUser.LoginId);
+        currentUser.LastLogin = DateTime.Now;
+        dbContext.SaveChanges();
+      }
+      else {
+        currentUser.Created = DateTime.Now;
+        currentUser.LastLogin = DateTime.Now;
+        currentUser.CanEdit = false;
+        currentUser.CanCreate = false;
+        currentUser.TenantAdmin = false;
+        dbContext.Users.Add(currentUser);
+        dbContext.SaveChanges();
+      }
+
+    }
+  }
+
+}
